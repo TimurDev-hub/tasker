febb8a218bce5492ac8706bba91ee1912390618b
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-16 18:37:17 +0300

    Primary commit

---
+++ app/src/shared/ErrorLogger.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Shared;
+
+use Throwable;
+
+class ErrorLogger
+{
+	private const LOCAL_LOG_FILE_PATH = '#';
+
+	private function getPrefix(): string
+	{
+		return 'PHP FATAL ERROR: ' . date('Y-m-d H:i:s') . ': ';
+	}
+
+	private function getInfo(Throwable $exc): string
+	{
+		return 'MESSAGE => ' . $exc->getMessage() . '; FILE=> ' . $exc->getFile() . '; LINE=> ' . $exc->getLine();
+	}
+
+	public static function handleError(Throwable $exc): void
+	{
+		$errorPrefix = self::getPrefix();
+		$errorInfo = self::getInfo(exc: $exc);
+
+		$errorMessage = $errorPrefix . $errorInfo . PHP_EOL;
+
+		error_log($errorMessage, 3, self::LOCAL_LOG_FILE_PATH);
+	}
+}
