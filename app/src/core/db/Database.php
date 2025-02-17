922aa889c345a728ddb23c058c34164c51dc2a28
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-17 22:51:16 +0300

    added Database.php

--- app/src/core/db/Database.php
+++ app/src/core/db/Database.php
@@ -2,7 +2,37 @@
 
 namespace Database;
 
+use PDO;
+use PDOException;
+use Shared\EnvParser;
+
 class Database
 {
-	
+	private PDO $pdo;
+
+	public function __construct()
+	{
+		$envParser = new EnvParser(__DIR__ . '/../../../.env');
+		$envFile = $envParser->parseFile();
+
+		if (!$envFile) throw new \Exception("Failed to load .env file");
+
+		$dbHost = $envFile['DB_HOST'];
+		$dbName = $envFile['DB_NAME'];
+		$dbUser = $envFile['DB_USER'];
+		$dbPassword = $envFile['DB_PASSWORD'];
+
+		try {
+			$this->pdo = new PDO("pgsql:host={$dbHost};dbname={$dbName};user={$dbUser};password={$dbPassword};");
+			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		} catch (PDOException $exc) {
+			throw new \Exception('Database connection failed: ' . $exc->getMessage());
+		}
+	}
+
+	public function returnPdo(): PDO
+	{
+		return $this->pdo;
+	}
 }
