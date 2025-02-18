c2a6f74ac2ddd52487153ecd8b8738fc830f3e8e
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-18 11:27:14 +0300

    created main controllers

--- app/src/modules/controllers/TaskController.php
+++ app/src/modules/controllers/TaskController.php
@@ -2,7 +2,69 @@
 
 namespace Controllers;
 
+use Database\Database;
+use Models\TaskModel;
+use Shared\ErrorLogger;
+use Throwable;
+
 class TaskContoller
 {
-	
+	public function createTask(): string
+	{
+		try {
+			$db = new Database();
+			$pdo = $db->returnPdo();
+
+			$taskModel = new TaskModel(pdo: $pdo, taskData: $_POST);
+
+			if ($taskModel->createTask()) {
+				return json_encode(['message' => 'Created successfully!']);
+			} else {
+				return json_encode(['error' => 'Failed to create task']);
+			}
+
+		} catch (Throwable $exc) {
+			ErrorLogger::handleError($exc);
+			return json_encode(['error' => 'Internal Server Error']);
+		}
+	}
+
+	public function loadTasks(): string
+	{
+		try {
+			$db = new Database();
+			$pdo = $db->returnPdo();
+
+			$taskModel = new TaskModel(pdo: $pdo, taskData: []);
+			$tasks = $taskModel->getTasks();
+
+			if (!$tasks) return json_encode(['error' => 'Internal Server Error']);
+
+			return json_encode(['tasks' => $tasks]);
+
+		} catch (Throwable $exc) {
+			ErrorLogger::handleError(exc: $exc);
+			return json_encode(['error' => 'Internal Server Error']);
+		}
+	}
+
+	public function deleteTask(): string
+	{
+		try {
+			$db = new Database();
+			$pdo = $db->returnPdo();
+
+			$taskModel = new TaskModel(pdo: $pdo, taskData: $_POST);
+
+			if ($taskModel->deleteTask()) {
+				return json_encode(['message' => 'Deleted successfully!']);
+			} else {
+				return json_encode(['error' => 'Failed to delete task']);
+			}
+
+		} catch (Throwable $exc) {
+			ErrorLogger::handleError(exc: $exc);
+			return json_encode(['error' => 'Internal Server Error']);
+		}
+	}
 }
--- app/src/modules/controllers/UserController.php
+++ app/src/modules/controllers/UserController.php
@@ -2,7 +2,50 @@
 
 namespace Controllers;
 
+use Database\Database;
+use Models\UserModel;
+use Shared\ErrorLogger;
+use Throwable;
+
 class UserController
 {
-	
+	public function createAccount(): string
+	{
+		try {
+			$db = new Database();
+			$pdo = $db->returnPdo();
+
+			$userModel = new UserModel(pdo: $pdo, userArray: $_POST);
+
+			if ($userModel->createAccount()) {
+				return json_encode(['message' => 'Created successfully!']);
+			} else {
+				return json_encode(['error' => 'Failed to create account']);
+			}
+
+		} catch (Throwable $exc) {
+			ErrorLogger::handleError(exc: $exc);
+			return json_encode(['error' => 'Internal Server Error']);
+		}
+	}
+
+	public function deleteAccount(): string
+	{
+		try {
+			$db = new Database();
+			$pdo = $db->returnPdo();
+
+			$userModel = new UserModel(pdo: $pdo, userArray: $_POST);
+
+			if ($userModel->deleteAccount()) {
+				return json_encode(['message' => 'Deleted successfully!']);
+			} else {
+				return json_encode(['error' => 'Failed to delete account']);
+			}
+
+		} catch (Throwable $exc) {
+			ErrorLogger::handleError(exc: $exc);
+			return json_encode(['error' => 'Internal Server Error']);
+		}
+	}
 }
