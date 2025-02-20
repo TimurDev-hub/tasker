520f91b947aa06cbbee9e4f08421984e555ceab7
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-20 21:15:11 +0300

    add new 'utils' dir

---
+++ app/src/shared/utils/EnvParser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Utils;
+
+use Throwable;
+
+class EnvParser
+{
+	private string $filePath;
+
+	public function __construct(string $filePath)
+	{
+		$this->filePath = $filePath;
+	}
+
+	private function loadFile(): array
+	{
+		return file($this->filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	}
+
+	public function parseFile(): array|false
+	{
+		$fileData = [];
+
+		try {
+			foreach ($this->loadFile() as $line) {
+				$linesArray = explode('=', $line, 2);
+	
+				$key = $linesArray[0];
+				$value = $linesArray[1];
+	
+				$fileData[$key] = $value;
+			}
+
+		} catch (Throwable) {
+			return false;
+		}
+
+		return $fileData;
+	}
+}
