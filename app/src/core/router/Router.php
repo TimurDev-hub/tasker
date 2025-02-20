abc03d95a7d568998df71d8738abb9e6c9ca4243
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-20 13:02:26 +0300

    updated Router.php

--- app/src/core/router/Router.php
+++ app/src/core/router/Router.php
@@ -6,37 +6,23 @@ use Shared\ErrorLogger;
 
 class Router
 {
-	public function routerStarter(string $uri, string $method): void
+	public function handleRequest(string $uri, string $methodType): void
 	{
 		try {
-			$uriP = explode('/', trim($uri, '/'));
-			$controllerName = ucfirst(array_shift($uriP)) . 'Controller';
+			$uriSegments = explode('/', trim($uri, '/'));
+			$controllerName = ucfirst(array_shift($uriSegments)) . 'Controller';
 
 			$controllerClass = '\\Controllers\\' . $controllerName;
 
 			if (!class_exists($controllerClass)) throw new \Exception('Controller not found: ' . $controllerName);
 
 			$controller = new $controllerClass();
+			$method = strtolower($methodType);
 
-			switch ($method) {
-				case 'GET':
-					$methodName = 'get';
-					break;
-				case 'POST':
-					$methodName = 'post';
-					break;
-				case 'PUT':
-					$methodName = 'put';
-					break;
-				case 'DELETE':
-					$methodName = 'delete';
-					break;
-			}
-
-			if (!method_exists($controller, $methodName)) throw new \Exception("Method not found: " . $methodName);
+			if (!method_exists($controller, $method)) throw new \Exception('Method not found: ' . $method);
 
 			header('Content-Type: application/json');
-			echo $controller->$methodName();
+			echo $controller->$method();
 
 		} catch (\Exception $exc) {
 			ErrorLogger::handleError(exc: $exc);
