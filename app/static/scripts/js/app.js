cec034f99b3b9d2d321dfd2dcf32e962dd39af27
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-23 20:19:16 +0300

    updated js-files

--- app/static/scripts/js/app.js
+++ app/static/scripts/js/app.js
@@ -1,2 +1,17 @@
-//import { headerNavbar, mainRegistration, mainLogin } from './templates.js';
-//import { getCookie, sendRegistrationData } from './functions.js';
+import { headerNavbar, mainRegistration, mainLogin } from './templates.js';
+import { getCookie } from './functions.js';
+
+const userId = getCookie('user_id');
+
+if (userId === null) {
+	document.getElementById('header-root').innerHTML = headerNavbar;
+	document.getElementById('main-root').innerHTML = mainLogin;
+
+	document.getElementById('registration-link').addEventListener('click', function() {
+		document.getElementById('main-root').innerHTML = mainRegistration;
+	});
+
+	document.getElementById('login-link').addEventListener('click', function() {
+		document.getElementById('main-root').innerHTML = mainLogin;
+	});
+}
