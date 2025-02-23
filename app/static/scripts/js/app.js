7f98c6d7794471de7f966d5eff49d4694000b981
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-23 12:33:55 +0300

    updated app.js

--- app/static/scripts/js/app.js
+++ app/static/scripts/js/app.js
@@ -7,17 +7,19 @@ function getCookie(name)
 	if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
+const header = document.getElementById('header-root');
+const main = document.getElementById('main-root');
 const userId = getCookie('user_id');
 
 if (userId == undefined) {
-	document.getElementById('header-root').innerHTML = headerNavbar;
-	document.getElementById('main-root').innerHTML = mainLogin;
+	header.innerHTML = headerNavbar;
+	main.innerHTML = mainLogin;
 
 	document.getElementById('registration-link').addEventListener('click', function() {
-		document.getElementById('main-root').innerHTML = mainRegistration;
-	})
+		main.innerHTML = mainRegistration;
+	});
 
 	document.getElementById('login-link').addEventListener('click', function() {
-		document.getElementById('main-root').innerHTML = mainLogin;
-	})
+		main.innerHTML = mainLogin;
+	});
 }
