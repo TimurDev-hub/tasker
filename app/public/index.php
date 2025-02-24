8cfff86757f2c12d4fb22d1210b0fdd693c2acf2
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-24 14:50:44 +0300

    updated index.php

--- app/public/index.php
+++ app/public/index.php
@@ -2,4 +2,15 @@
 
 require_once __DIR__ . "../../../vendor/autoload.php";
 
-require_once __DIR__ . '../../src/modules/views/index.html';
+use Router\Router;
+
+$uri = $_SERVER['REQUEST_URI'];
+$methodType = $_SERVER['REQUEST_METHOD'];
+
+if ($uri === '/authentication' || $uri === '/user' ) {
+	$router = new Router();
+	$router->handleRequest(uri: $uri, methodType: $methodType);
+
+} else {
+	require_once __DIR__ . '../../src/modules/views/index.html';
+}
