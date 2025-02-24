293b06691b58d73dca045849981534c6be463007
TimurDev <dev.schuchkin.timur@gmail.com>
2025-02-24 14:44:52 +0300

    fixed Router.php

--- app/src/core/router/Router.php
+++ app/src/core/router/Router.php
@@ -12,7 +12,10 @@ class Router
 			$uriSegments = explode('/', trim($uri, '/'));
 			$controllerName = ucfirst(array_shift($uriSegments)) . 'Controller';
 
-			$controllerClass = '\\Controllers\\' . $controllerName;
+			// fixing
+			file_put_contents(__DIR__ . '/../../../../index.txt', $controllerName . PHP_EOL, FILE_APPEND);
+
+			$controllerClass = 'Controllers\\' . $controllerName;
 
 			if (!class_exists($controllerClass)) throw new \Exception('Controller not found: ' . $controllerName);
 
@@ -21,14 +24,14 @@ class Router
 
 			if (!method_exists($controller, $method)) throw new \Exception('Method not found: ' . $method);
 
-			header('Content-Type: application/json');
+			header("Content-Type: application/json");
 			http_response_code(200);
 			echo $controller->$method();
 
 		} catch (\Throwable $exc) {
 			ErrorLogger::handleError(exc: $exc);
 			http_response_code(404);
-			header('Content-Type: application/json');
+			header("Content-Type: application/json");
 			echo json_encode(['error' => $exc->getMessage()]);
 		}
 	}
