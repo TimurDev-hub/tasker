f7dc407e904cf3c9b0fbd464941c48a9bc18500d
TimurDev <dev.schuchkin.timur@gmail.com>
2025-03-01 22:07:34 +0300

    updated Router.php

--- app/src/core/router/Router.php
+++ app/src/core/router/Router.php
@@ -6,28 +6,63 @@ use Utils\ErrorLogger;
 
 class Router
 {
-	public function handleRequest(string $uri): void
+	private static function prepareUri(string $uri): string
+	{
+		return trim(str_replace('/api', '', $uri), '/');
+	}
+
+	private static function getControllersMap(): array
+	{
+		return [
+			'authentication' => 'Controllers\\AuthenticationController',
+			'user' => 'Controllers\\UserController',
+			'task' => 'Controllers\\TaskController'
+		];
+	}
+
+	private static function getController(string $uri): string
+	{
+		return self::getControllersMap()[self::prepareUri(uri: $uri)];
+	}
+
+	private static function getMethod(): array
+	{
+		return [
+			'authentication' => [
+				'POST' => 'login',
+				'DELETE' => 'logout'
+			],
+			'user' => [
+				'POST' => 'registerUser',
+				'DELETE' => 'deleteUser'
+			],
+			'task' => [
+				'POST' => 'createTask',
+				'GET' => 'getTasks',
+				'DETELE' => 'deleteTask'
+			]
+		];
+	}
+
+	public function handleRequest(string $uri, string $methodType): void
 	{
 		try {
-			$uriSegments = explode('/', trim($uri, '/'), 2);
-
-			$controllerClass = 'Controllers\\' . ucfirst($uriSegments[0]) . 'Controller';
+			$resourceName = self::prepareUri(uri: $uri);
+			$controllerClass = self::getController(uri: $uri);
 
 			if (!class_exists($controllerClass)) throw new \Exception('Controller not found: ' . $controllerClass);
 
 			$controller = new $controllerClass();
-			$method = $uriSegments[1];
+			$method = self::getMethod()[$resourceName][$methodType];
 
 			if (!method_exists($controller, $method)) throw new \Exception('Method not found: ' . $method);
 
-			header("Content-Type: application/json");
 			http_response_code(200);
 			echo $controller->$method();
 
 		} catch (\Throwable $exc) {
 			ErrorLogger::handleError(exc: $exc);
 			http_response_code(404);
-			header("Content-Type: application/json");
 			echo json_encode(['error' => $exc->getMessage()]);
 		}
 	}
