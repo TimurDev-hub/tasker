4ecdd31bcf94dfc95fc62304f77ca999be097f18
TimurDev <dev.schuchkin.timur@gmail.com>
2025-03-01 21:07:38 +0300

    updated index.php, added header of content-type

--- app/public/index.php
+++ app/public/index.php
@@ -1,14 +1,17 @@
 <?php
 
+header("Content-Type: application/json");
+
 require_once __DIR__ . "../../../vendor/autoload.php";
 
 use Router\Router;
 
 $uri = $_SERVER['REQUEST_URI'];
+$methodType = $_SERVER['REQUEST_METHOD'];
 
-if (strpos($uri, '/api') !== false) {
+if (strpos($uri, '/api') !== false && strpos($uri, '/api') === 0) {
 	$router = new Router();
-	$router->handleRequest(uri: $uri);
+	$router->handleRequest(uri: $uri, methodType: $methodType);
 
 } else {
 	require_once __DIR__ . '../../src/modules/views/index.html';
