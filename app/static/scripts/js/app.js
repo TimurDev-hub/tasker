8ddb2e1bfe0df7a57c68dd39d315389037cc2f5b
TimurDev <dev.schuchkin.timur@gmail.com>
2025-03-02 22:03:14 +0300

    updated JS-files

--- app/static/scripts/js/app.js
+++ app/static/scripts/js/app.js
@@ -1,22 +1,22 @@
 import { headerNavbar, headerUserblock, mainLogin, mainRegistration, mainCreate, taskArea } from './templates.js';
 import { getCookie } from './utils.js';
-import { setupRegistrationForm, setupLoginForm } from './functions.js';
+import { registration, login } from './functions.js';
 
 const userId = getCookie('user_id');
 
 
-if (!userId === null) {
+if (userId === null) {
 	document.getElementById('header-root').innerHTML = headerNavbar;
 	//document.getElementById('main-root').innerHTML = mainLogin;
 
 	document.getElementById('registration-link').addEventListener('click', function() {
 		document.getElementById('main-root').innerHTML = mainRegistration;
-		setupRegistrationForm();
+		registration();
 	});
 
 	document.getElementById('login-link').addEventListener('click', function() {
 		document.getElementById('main-root').innerHTML = mainLogin;
-		setupLoginForm();
+		login();
 	});
 
 } else {
