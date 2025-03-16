4088f93c9d0deee203fb2ed963d367ca2770bd89
TimurDev <dev.schuchkin.timur@gmail.com>
2025-03-16 17:26:21 +0300

    updated scripts

--- app/static/scripts/js/Templates.js
+++ app/static/scripts/js/Templates.js
@@ -37,11 +37,11 @@ export class Templates {
 				<form id="loginForm" class="sections__form">
 					<fieldset class="sections__field sections__field--userforms">
 						<label for="username" class="sections__title">Name</label>
-						<input id="username" name="username" name="user_name" type="text" class="sections__text sections__text--userforms main__inputs" placeholder="Your username is..." maxlength="16" required>
+						<input id="username" name="username" autocomplete="off" type="text" class="sections__text sections__text--userforms main__inputs" placeholder="Your username is..." maxlength="16" required>
 					</fieldset>
 					<fieldset class="sections__field">
 						<label for="password" class="sections__title">Password</label>
-						<input id="password" name="password" name="user_password" type="password" class="sections__text sections__text--userforms main__inputs" placeholder="Your password is..." maxlength="24" required>
+						<input id="password" name="password" autocomplete="off" type="password" class="sections__text sections__text--userforms main__inputs" placeholder="Your password is..." maxlength="24" required>
 					</fieldset>
 					<button type="submit" class="sections__submit">Submit</button>
 					<span id="formMessageArea"></span>
@@ -56,11 +56,11 @@ export class Templates {
 				<form id="registrationForm" class="sections__form">
 					<fieldset class="sections__field sections__field--userforms">
 						<label for="username" class="sections__title">Name</label>
-						<input id="username" name="username" name="user_name" type="text" class="sections__text sections__text--userforms main__inputs" placeholder="Think of your own name!" maxlength="12" required>
+						<input id="username" name="username" autocomplete="off" type="text" class="sections__text sections__text--userforms main__inputs" placeholder="Think of your own name!" maxlength="12" required>
 					</fieldset>
 					<fieldset class="sections__field">
 						<label for="password" class="sections__title">Password</label>
-						<input id="password" name="password" name="user_password" type="password" class="sections__text sections__text--userforms main__inputs" placeholder="...and a strong password!" maxlength="24" required>
+						<input id="password" name="password" autocomplete="off" type="password" class="sections__text sections__text--userforms main__inputs" placeholder="...and a strong password!" maxlength="24" required>
 					</fieldset>
 					<button type="submit" class="sections__submit">Submit</button>
 					<span id="formMessageArea"></span>
@@ -75,11 +75,11 @@ export class Templates {
 				<form id="taskCreateForm" class="sections__form">
 					<fieldset class="sections__field">
 						<label for="title" class="sections__title">Title</label>
-						<input id="title" name="task_title" type="text" class="sections__text main__inputs" placeholder="Your task\`s title is..." maxlength="16" required>
+						<input id="title" name="task_title" autocomplete="off" type="text" class="sections__text main__inputs" placeholder="Your task\`s title is..." maxlength="16" required>
 					</fieldset>
 					<fieldset class="sections__field">
 						<label for="task" class="sections__title">Task</label>
-						<textarea id="task" name="task_text" class="sections__text sections__text--task main__inputs" placeholder="Write what you want!" maxlength="64" required></textarea>
+						<textarea id="task" name="task_text" autocomplete="off" class="sections__text sections__text--task main__inputs" placeholder="Write what you want!" maxlength="64" required></textarea>
 					</fieldset>
 					<button type="submit" class="sections__submit">Submit</button>
 					<span id="formMessageArea"></span>
